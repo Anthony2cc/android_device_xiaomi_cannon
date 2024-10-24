57d61d03d91e9f238a80735bb6c8da52f4fc90aa
Anthony2cc <Anthony2cc@gmail.com>
2024-10-24 08:57:45 +0800

    Update AndroidProducts.mk
    Signed-off-by: Anthony2cc <Anthony2cc@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_cannon.mk
-
-COMMON_LUNCH_CHOICES := \
-    lineage_cannon-user \
-    lineage_cannon-userdebug \
-    lineage_cannon-eng
+    $(LOCAL_DIR)/aosp_cannon.mk
